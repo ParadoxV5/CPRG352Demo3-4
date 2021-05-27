2f7a6e85efc00eb5c10dd51ee32aec7e75efaa9a
ParadoxV5 <paradox.ver5@gmail.com>
2021-05-27 17:00:27 -0600

    Day 2 (:

--- src/java/servlet/HelloWorldServlet.java
+++ src/java/servlet/HelloWorldServlet.java
@@ -6,12 +6,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends jakarta.servlet.http.HttpServlet {
+  private static final String ERROR_MESSAGE = "Please enter a name (:";
   private static final long serialVersionUID = 1;
   @Override protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     getServletContext().getRequestDispatcher("/WEB-INF/HelloWorldForm.jsp").forward(request, response);
   }
   @Override protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    request.setAttribute("name", request.getParameter("name"));
-    getServletContext().getRequestDispatcher("/WEB-INF/HelloWorld.jsp").forward(request, response);
+    String name = request.getParameter("name");
+    request.setAttribute("name", name);
+    
+    if(name == null || name.trim().isEmpty()) { // Validation
+      request.setAttribute("error_message", ERROR_MESSAGE);
+      doGet(request, response); // Reload page by redirect to original form page
+      return; // Stop execution – important~
+    }
+    
+    getServletContext().getRequestDispatcher("/WEB-INF/HelloWorld.jsp").forward(request, response); // Continue
   }
 }
--- web/WEB-INF/HelloWorldForm.jsp
+++ web/WEB-INF/HelloWorldForm.jsp
@@ -5,10 +5,13 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Hello World Form</title>
   </head>
-  <body><form method="POST" action="HelloWorld">
+  <body>
     <h1>Hello World Form</h1>
+    <form method="POST" action="HelloWorld">
     <label for="name">Name</label>
-    <input id="name" name="name" type="text" autocomplete="nickname" required>
+    <input id="name" name="name" type="text" autocomplete="nickname" value="${name}">
     <input type="submit">
-  </form></body>
+  </form>
+  <p id="error_message" style="font-size: 75%; color: red">${error_message}</p>
+  </body>
 </html>
